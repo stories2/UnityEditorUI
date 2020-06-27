949039b615c0a0cb3c6409e3bd47b2651fcdbee1
Kim HyunWoo <stories2@naver.com>
2020-06-28 00:15:59 +0900

    create menu item

---
+++ Assets/Scripts/MenuItemTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+// https://docs.unity3d.com/ScriptReference/MenuItem.html
+public class MenuItemTest : MonoBehaviour
+{
+    [MenuItem("MyMenu/Do Something")]
+    static void DoSomething()
+    {
+        Debug.Log("Doing Something...");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
